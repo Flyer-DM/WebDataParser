ac7a8b4d0029e4d0f5e2434b6ec9a1ff9129467c
Dmitry <lexa.gorky@yandex.ru>
2024-07-25 23:30:57 +0300

    Поменял логику базового экспортёра

--- data_export/base_export.py
+++ data_export/base_export.py
@@ -7,53 +7,52 @@ from typing import List, Dict
 
 class BaseExport(ABC):
 
-    __version__ = "0.1"
+    __version__ = "0.2"
 
-    @abstractmethod
-    def set_base_name(self):
+    def __set_base_name(self) -> str:
         """Получение имени файла по умолчанию, если пользователь не указал его явно, а именно название вебсайта, по
         которому производился парсинг и дата и время сохранения файла
-        version = 0.1
+        version = 0.1.1
         """
         export_time = strftime("%Y-%m-%d %H %M %S", gmtime())
         base_name = f"{self.website} {export_time}"
         return base_name
 
-    @abstractmethod
-    def set_base_path(self):
+    @staticmethod
+    def __set_base_path() -> str:
         """Получение пути для сохранения файла по умолчанию, если пользователь не указал его явно, а именно папка
         Загрузки
-        version = 0.1
+        version = 0.1.1
         """
         download_path = str(os.path.join(Path.home(), "Downloads"))
         return download_path
 
-    @abstractmethod
-    def set_nullable_values(self):
+    def __set_nullable_values(self) -> None:
         """Установка имени файла и пути для его сохранения по умолчанию, если они не введены
-        version = 0.1
+        version = 0.1.1
         """
-        self.name = self.set_base_name() if self.name is None else self.name
-        self.path = self.set_base_path() if self.path is None else self.path
+        self.name = self.__set_base_name() if self.name is None else self.name
+        self.path = self.__set_base_path() if self.path is None else self.path
 
     def __init__(self, data: List[Dict], website: str, name: str = None, path: str = None):
-        """version = 0.1"""
+        """version = 0.2"""
         self.website = website
         self.data = data
         self.name = name
         self.path = path
+        self.__set_nullable_values()
 
     @property
     @abstractmethod
-    def extension(self):
+    def extension(self) -> str:
         """Расширение файла - обязательное свойство класса
-        version = 0.1
+        version = 0.1.1
         """
         ...
 
     @abstractmethod
-    def export_data(self):
+    def export_data(self) -> None:
         """Функция экспорта данных
-        version = 0.1
+        version = 0.1.1
         """
         ...
--- data_export/txt_export.py
+++ data_export/txt_export.py
@@ -4,45 +4,24 @@ from typing import List, Dict
 
 class TXTExport(BaseExport):
 
-    __version__ = "0.1"
-
-    def set_base_name(self):
-        """Получение имени файла по умолчанию, если пользователь не указал его явно, а именно название вебсайта, по
-        которому производился парсинг и дата и время сохранения файла
-        version = 0.1
-        """
-        return super().set_base_name()
-
-    def set_base_path(self):
-        """Получение пути для сохранения файла по умолчанию, если пользователь не указал его явно, а именно папка
-        Загрузки
-        version = 0.1
-        """
-        return super().set_base_path()
-
-    def set_nullable_values(self):
-        """Установка имени файла и пути для его сохранения по умолчанию, если они не введены
-        version = 0.1
-        """
-        return super().set_nullable_values()
+    __version__ = "0.2"
 
     def __init__(self, data: List[Dict], website: str, name: str = None, path: str = None):
         """version = 0.1"""
         super().__init__(data, website, name, path)
         self._extension = ".txt"
+        self.file_name = f"{self.path}/{self.name}{self.extension}"
 
     @property
-    def extension(self):
+    def extension(self) -> str:
         """Расширение файла - txt
-        version = 0.1
+        version = 0.1.1
         """
         return self._extension
 
-    def export_data(self):
+    def export_data(self) -> None:
         """Функция экспорта данных
-        version = 0.1
+        version = 0.1.1
         """
-        self.set_nullable_values()
-        file_name = f"{self.path}/{self.name}{self.extension}"
-        with open(file_name, 'w') as f:
+        with open(self.file_name, 'w') as f:
             print(self.data, file=f)
